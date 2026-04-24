764fd6bc449dbe5f5141e03c22afdcbabe115a5f
Stefan Worzalla <worzst@users.noreply.github.com>
2026-04-24 16:01:35 +0700

    add sitemap and robots txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.ictira.com',
+  site: "https://www.ictira.com",
+  integrations: [sitemap()],
 });
